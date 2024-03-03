7e1572c2a1c1147c59bdf8972b5d8abdec863ca3
Jackie <jli522543@gmail.com>
2024-03-03 14:27:39 -0800

    Set up database

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 To run
 
 flask --app flaskr run --debug
+
+To initialize database
+
+flask --app flaskr init-db
--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,11 +1,12 @@
 import os
 
 from flask import Flask
-
+from . import db
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
+    db.init_app(app)
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
